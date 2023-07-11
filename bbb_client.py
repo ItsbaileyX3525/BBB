25b3541d22c841901aa7e112323dd5e64b529bef
Bailey Miles <baileyleah542@gmail.com>
2023-07-11 18:04:44 +0100

    update v

--- bbb_client.py
+++ bbb_client.py
@@ -110,7 +110,7 @@ class Welcome():
         self.parent=camera.ui
         self.model='quad'
         self.color=color.white
-        self.version = "1.5.2"
+        self.version = "1.6.0"
         self.versionGet = requests.get("https://raw.githubusercontent.com/ItsbaileyX3525/BBB/main/version.txt")
         if self.versionGet.status_code != 200:
             print("Failed to retrieve file. Status code:", self.versionGet.status_code)
