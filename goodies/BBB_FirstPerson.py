0191d0b9638e267f833b4bdd88168acd3fb3ac71
Bailey Miles <baileyleah542@gmail.com>
2023-07-15 02:25:17 +0100

    quick update before bobo time

--- goodies/BBB_FirstPerson.py
+++ goodies/BBB_FirstPerson.py
@@ -1,26 +1,32 @@
 from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
 from networking import *
-from FPC import *
 
 app=Ursina(borderless=False)
 start_text = "Press H to host or C to connect."
 status_text = Text(text=start_text, origin=(0, 0), z=1)
 
+class otherPlayerModel(Entity):
+    def __init__(self):
+        super().__init__(self)
+        self.model='cube'
+        self.scale_y=2
+        self.color=color.blue
+        self.face=Entity(parent=self,scale_y=.25,scale_x=.5,z=.05,y=.25,model='cube',color=color.green)
+        
+
 ground = Entity(model='plane',scale_x=100,scale_z=100,texture='grass',collider='box')
 mainPlayer = FirstPersonController(model='cube',color=color.red)
-otherPlayer = Entity(model='cube',color=color.blue)
+otherPlayer = otherPlayerModel()
 peer = RPCPeer()
 update_rate = 1.0 / 20.0
 update_timer = 0.0
 
 @rpc(peer)
-def set_position(connection, time_received, position: Vec3):
+def set_position(connection, time_received, position: Vec3, rotation: Vec3):
     otherPlayer.x = position.x
     otherPlayer.y = position.y
     otherPlayer.z = position.z 
-
-@rpc(peer)
-def set_rotation(connection, time_received, rotation: Vec3):
     otherPlayer.rotation_x = rotation.x
     otherPlayer.rotation_y = rotation.y
     otherPlayer.rotation_z = rotation.z
@@ -45,8 +51,7 @@ def update():
         status_text.text = "Host"
     update_timer += time.dt
     if peer.is_running() and peer.connection_count() > 0:
-        peer.set_position(peer.get_connections()[0], Vec3(mainPlayer.x, mainPlayer.y,mainPlayer.z))
-        peer.set_rotation(peer.get_connections()[0], Vec3(mainPlayer.rotation_x, mainPlayer.rotation_y,mainPlayer.rotation_z))
+        peer.set_position(peer.get_connections()[0], Vec3(mainPlayer.x, mainPlayer.y,mainPlayer.z), Vec3(mainPlayer.rotation_x, mainPlayer.rotation_y,mainPlayer.rotation_z))
     update_timer = 0.0
 def input(key):
     global mainPlayer,otherPlayer
